cb4b4ce552123797e12f42aaf202d2f350b144da
Uriel Lopes <uriel.lopes@imd.ufrn.br>
2019-11-18 16:24:36 -0300

    template de array

--- main.cpp
+++ main.cpp
@@ -19,10 +19,6 @@ class Type1 : public Parent {
     	virtual void print() = 0;
 };
 
-// void Type1::print() {
-// 	std::cout << "Tipo 1" << std::endl;
-// }
-
 class Type1Filho : public Type1 {
 private:
 	int asd;
@@ -58,20 +54,75 @@ void Type3::print() {
 	std::cout << "Tipo 3" << std::endl;
 }
 
-// template <typename T>
-// class Pilha
+template <typename T>
+class Pilha {
+private:
+	T* m_elementos; // Elementos armazenados na pilha
+	int m_tamanho; // Qtde atual de elementos
+	int m_capacidade; // Qtde Max de elementos
+public:
+	Pilha (int n_capacidade = 50);
+	~Pilha ();
+	bool empty ();
+	bool full ();
+	T& top ();
+	int push ( T novo );
+	int pop ();
+	int size();
+	T &operator[] (int);
+};
+
+template <typename T>
+Pilha<T>::Pilha (int n_capacidade):
+m_tamanho(0), m_capacidade(n_capacidade)
+{
+	m_elementos = new T[n_capacidade];
+}
+template <typename T>
+Pilha<T>::~Pilha ()
+{
+	delete [] m_elementos;
+}
+
+template <typename T>
+bool Pilha<T>::full ()
+{
+	return m_tamanho == m_capacidade;
+}
+
+template <typename T>
+int Pilha<T>::push ( T novo )
+{
+	if (full())
+		return 0;
+	m_elementos[m_tamanho++] =
+	novo;
+	return 1;
+}
+
+template <typename T>
+T &Pilha<T>::operator[](int index) 
+{ 
+    if (index >= m_tamanho) 
+    { 
+        std::cout << "Array index out of bound, exiting"; 
+        exit(0); 
+    } 
+    return m_elementos[index]; 
+}
+
 
 int main() {
 
-	std::vector<Parent*> vec;
+	Pilha<Parent*> vec;
 
-	vec.push_back(new Type1Filho);
-	vec.push_back(new Type2);
-	vec.push_back(new Type3);
+	vec.push(new Type1Filho);
+	// vec.push(new Type2);
+	// vec.push(new Type3);
 
 	vec[0]->print();
-	vec[1]->print();
-	vec[2]->print();
+	// vec[1]->print();
+	// vec[2]->print();
 
 	std::cout << "ola" << std::endl;
 
