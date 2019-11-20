ef37194386d8175219d9b981c8ddab4c062ffa51
uriellopes <urielrlps@gmail.com>
2019-11-20 20:52:59 -0300

    Ler dados de funcionarios de arquivo

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -20,23 +20,27 @@ void lerDados(vector<Tratador> &t, vector<Veterinario> &v) {
     
     ifstream file_animais("funcionarios.csv");
 
-    // vector<string> values;
-    // string line, value;
+    vector<string> values;
+    string line, value;
 
-    // while(!file_animais.eof()) {
-    //     getline(file_animais, line);
-    //     if ( !line.empty() ) {
-    //         stringstream temp(line);
-    //         values.clear();
-    //         while(getline(temp, value, ';')) {
-    //             values.push_back(value);
-    //         }
-    //         animais.push_back(
-    //         	Animal(stoi(values[0]), values[1], values[2], values[3], *values[4].c_str(), 
-    //         		stod(values[5]), values[6], stoi(values[7]), stoi(values[8]), values[9])
-    //         	);
-    //     }
-    // }
+    while(!file_animais.eof()) {
+        getline(file_animais, line);
+        if ( !line.empty() ) {
+            stringstream temp(line);
+            values.clear();
+            while(getline(temp, value, ';')) {
+                values.push_back(value);
+            }
+            switch (stoi(values[0])) {
+            case 0:
+                t.push_back(Tratador(stoi(values[1]), values[2], values[3], stoi(values[4]), values[5], *values[6].c_str(), values[7], stoi(values[8])));
+                break;
+            case 1:
+                v.push_back(Veterinario(stoi(values[1]), values[2], values[3], stoi(values[4]), values[5], *values[6].c_str(), values[7], values[8]));
+                break;
+            }
+        }
+    }
 
     file_animais.close();
 }
@@ -62,9 +66,6 @@ int main() {
 	vector<Veterinario> veterinarios;
 
 	lerDados(tratadores, veterinarios);
-
-	tratadores.push_back(Tratador(1,"João Alberto","007.404.200-98",45,"AB",'-',"Répteis e Aves",1));
-	veterinarios.push_back(Veterinario(2,"Daniel Oscar","123.456.789-10",30,"O",'+',"Felinos","CRMV-GO 0406"));
 
 	salvarDados(tratadores, veterinarios);
 
